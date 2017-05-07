be82c04cc4c8ad1f3fa635c4439b5b005c52957e
Mark Klass <mark@markbookair.local>
2017-05-07 16:51:19 +0800

    Improved folder structure

---
+++ Assignment 03/src/IDS.cpp
@@ -0,0 +1,9 @@
+//
+//  IDS.cpp
+//  Assignment 03
+//
+//  Created by Mark Klass on 7/5/17.
+//  Copyright © 2017 Juicy Apps. All rights reserved.
+//
+
+#include "IDS.hpp"
---
+++ Assignment 03/src/IDS.hpp
@@ -0,0 +1,14 @@
+//
+//  IDS.hpp
+//  Assignment 03
+//
+//  Created by Mark Klass on 7/5/17.
+//  Copyright © 2017 Juicy Apps. All rights reserved.
+//
+
+#ifndef IDS_hpp
+#define IDS_hpp
+
+#include <stdio.h>
+
+#endif /* IDS_hpp */
